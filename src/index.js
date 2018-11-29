be4b2bfbb86cbb21d9bdec67115cc65e21129b5c
Paul van Duyn <paul.vanduyn@gmail.com>
2018-11-29 16:26:37 -0500

    webpack up and running, starting to animate a player

---
+++ src/index.js
@@ -0,0 +1,55 @@
+import DrawPlayer from './draw_player.js';
+// import LeftSidebar from './leftsidebar_container';
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const canvas = document.getElementById('kukulkanvas');
+  const context = canvas.getContext('2d');
+  canvas.width = 960;
+  canvas.height = 640;
+
+  // const game = new Game();
+  // new GameView(game, ctx).start();
+
+  setInterval(DrawPlayer(context), 10);
+
+});
+
+// const draw = (context, maxHeight=50, angle=0, base=220) => () => {
+//   console.log('refreshing');
+//
+//   const height = maxHeight - 10*Math.cos(angle);
+//   y = base + 10*Math.cos(angle);
+//   angle += 0.1;
+//
+//   context.clearRect(0, 0, 960, 640);
+//
+//   context.beginPath();
+//
+//   context.rect(480, y, 30, height);
+//   context.fillStyle = "#FFFFFF";
+//   context.fill();
+//
+//   context.closePath();
+//
+// };
+
+
+
+
+
+
+// context.arc(480, 320, 320, start_angle, end_angle, false);
+// context.fillStyle = "#FFFFFF";
+// context.fill();
+
+// context.rect(center_x, center_y, width, height);
+// context.arc(center_x, center_y, radius, start_angle, end_angle, reverse?)
+
+// draw just a line
+// context.strokeStyle = "rgba(255, 0, 255, 50)";
+// context.stroke();
+
+// as opposed to stroke for outlines...
+// context.fillStyle = "#FFFFFF";
+// context.fill();
